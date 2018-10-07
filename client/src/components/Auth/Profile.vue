db76840effbe088a16e740261c74b17ba8a319cc
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-10-07 17:14:57 -0500

    Agregando modal para editar publicaciones

--- client/src/components/Auth/Profile.vue
+++ client/src/components/Auth/Profile.vue
@@ -56,7 +56,7 @@
       </v-layout>
     </v-container>
 
-    <v-container class="mt-2" v-else>
+    <v-container class="mt-1" v-else>
       <v-flex xs12>
         <h2 class="font-weight-light">Tus publicaciones
           <span class="font-weight-regular">({{userPosts.length}})</span>
@@ -65,19 +65,63 @@
       <v-layout row wrap>
         <v-flex xs12 sm3 v-for="post in userPosts" :key="post._id">
           <v-card class="mt-3 ml-1 mr-2" hover>
-            <v-btn color="info" small dark>
-              <v-icon>edit</v-icon>
-            </v-btn>
-            <v-btn color="error" small dark>
-              <v-icon>delete</v-icon>
-            </v-btn>
-
             <v-card-media height="30vh" :src="post.imageUrl"></v-card-media>
             <v-card-text>{{post.title}}</v-card-text>
+            <v-btn color="accent" small dark class="mb-3" @click="editPostDialog = true">
+              <v-icon>edit</v-icon>
+            </v-btn>
+            <v-btn color="error" small dark class="mb-3">
+              <v-icon>delete</v-icon>
+            </v-btn>
           </v-card>
         </v-flex>
       </v-layout>
     </v-container>
+
+    <!-- modal para editar publicación -->
+    <v-dialog xs12 sm6 offset-sm3 persistent v-model="editPostDialog">
+      <v-card>
+        <v-card-title class="headline grey lighten-2">Editar publicación</v-card-title>
+        <v-container>
+          <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleUpdateUserPost">
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field :rules="titleRules" v-model="title" label="Post Título" type="text" required></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field :rules="urlImageRules" v-model="imageUrl" label="URL de la imagen" type="text" required></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <img :src="imageUrl" height="300px">
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-select v-model="categories" :rules="categoriesRules" :items="['Vacaciones ', 'Playa ', 'Bosque', 'Familiar', 'Aniversario', 'Luna de miel', 'Paisaje', 'Relax', 'Extranjero', 'Aventura', 'Tour']" multiple label="Categorías"></v-select>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-textarea :rules="descriptionRules" v-model="description" label="Descripción" type="text" required></v-textarea>
+              </v-flex>
+            </v-layout>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn type="submit" class="success--text" flat>Guardar</v-btn>
+              <v-btn class="error--text" flat @click="editPostDialog = false">Cancel</v-btn>
+            </v-card-actions>
+
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -86,6 +130,32 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "Profile",
+  data() {
+    return {
+      editPostDialog: false,
+      isFormValid: true,
+      title: "",
+      imageUrl: "",
+      categories: [],
+      description: "",
+      titleRules: [
+        title => !!title || "Completa este campo",
+        title =>
+          title.length < 30 || "El título debe tener menos de 30 caracteres"
+      ],
+      descriptionRules: [
+        description => !!description || "Completa este campo",
+        description =>
+          description.length < 200 ||
+          "La descripción debe tener menos de 200 caracteres"
+      ],
+      urlImageRules: [urlImage => !!urlImage || "Completa este campo"],
+      categoriesRules: [
+        categories =>
+          categories.length >= 1 || "Debes marcar al menos una categoría"
+      ]
+    };
+  },
   computed: {
     ...mapGetters(["user", "userFavorites", "userPosts"])
   },
@@ -97,7 +167,8 @@ export default {
       this.$store.dispatch("getUserPosts", {
         userId: this.user._id
       });
-    }
+    },
+    handleUpdateUserPost() {}
   }
 };
 </script>
